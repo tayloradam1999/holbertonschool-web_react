2aced5574dd782ce3ce8c6a34c871cc516721a93
Adam Taylor <tayloradam1999@gmail.com>
2021-07-23 02:04:48 +0000

    Pushing updated file for task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -14,3 +14,27 @@ interface Directors extends Teacher {
 export default function printTeracher({firstName, lastName}: Teacher) {
   return (`${firstName.slice(0, 1)}. ${lastName})`);
 }
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+interface StudentInterface {
+    firstName: string;
+    lastName: string;
+}
+
+interface StudentConstructor {
+    new (firstName: string, lastName: string): Student;
+}
+
+class StudentClass implements StudentInterface {
+  constructor(firstName: string, lastName: string) {
+    }
+    workOnHomework() {
+      return 'Currently Working';
+    }
+    displayName() {
+      return this.firstName;
+    }
+}
