539c3482c00313255d42a5d9bb9e3656c7fce339
Adam Taylor <tayloradam1999@gmail.com>
2021-07-23 02:21:23 +0000

    Pushing updated files for tasks and new dir for task 11

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -34,9 +34,29 @@ class Teacher implements TeacherInterface {
   }
 }
 
-export default function createEmployee(salary: number | string) {
+function createEmployee(salary: number | string) {
   if (typeof salary === 'number' && salary < 500) {
     return new Teacher();
   }
   return new Director();
+}
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee instanceof Director);
+}
+
+function executeWork(employee) {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return employee.workTeacherTasks();
+}
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass) {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+  return 'Teaching History';
 }
