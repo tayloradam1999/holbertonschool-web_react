d02f8393baa7cefcae434169739fa65f91154fc4
Adam Taylor <tayloradam1999@gmail.com>
2022-03-08 15:03:19 -0600

    Push all files for task 2

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,24 @@
+// interesting jquery import thanks to babel
+import $ from 'jquery';
+// import lodash
+import _ from 'lodash';
+// styles
+import '../css/main.css';
+
+// append elements
+$('body').append(`<div id="logo"></div>`);
+$('body').append(`<p>Holberton Dashboard</p>`);
+$('body').append(`<p>Dashboard data for the students</p>`);
+$('body').append(`<button>Click here to get started</button>`);
+$('body').append(`<p id="count"></p>`);
+$('body').append(`<p>Copyright - Holberton School</p>`);
+
+// bind lodash's debounce to the button
+$('button').on('click', _.debounce(updateCounter, 500));
+
+let count  = 0;
+// function to update the counter
+function updateCounter() {
+	count++;
+	$('#count').text(`${count} clicks on the button`);
+}
---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'production',
+	entry: './js/dashboard_main.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'public')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.(jpe?g|png|gif|jpg|svg)$/i,
+				use: ['file-loader',
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							bypassonDebug: true,
+							disable: true,
+						},
+					},
+				],
+			}
+		]
+	}
+};
