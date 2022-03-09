4762b25a4157a73bb6bb1ceb796a14bf95d28a48
Adam Taylor <tayloradam1999@gmail.com>
2022-03-09 16:11:51 -0600

    Task 8 - Empty Webpack Project

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: [
+					'file-loader',
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							bypassOnDebug: true,
+							disable: true
+						},
+					},
+				],
+			},
+		]
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		hot: true,
+		static: path.join(__dirname, './dist'),
+		compress: true,
+		port: 3000,
+	},
+};
