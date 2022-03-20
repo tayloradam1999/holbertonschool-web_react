9bb590faef02bd08d7d434627f45ebe56b5c15dc
Adam Taylor <tayloradam1999@gmail.com>
2022-03-20 14:16:53 -0500

    Push Task 1 with all functionality minus tests

---
+++ 0x03-React_component/task_1/dashboard/src/App/App.test.js
@@ -0,0 +1,74 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import App from './App';
+import Login from '../Login/Login';
+
+
+// shallow render app component
+describe('<App />', () => {
+	it('Tests that App renders without crashing', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.exists()).toBe(true);
+	})
+
+	it('Contains Header component', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('Header').length).toBe(1);
+	})
+
+	it('Contains Login component', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.contains(<Login />)).toBe(true);
+	})
+
+	it('Contains Footer component', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('Footer').length).toBe(1);
+	})
+
+	it('Tests that CourseList is not displayed', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('CourseList').length).toBe(0);
+	})
+});
+
+
+// describe case when isLoggedIn is true
+describe('<App />', () => {
+	it('Tests that the Login component is not rendered', () => {
+		const wrapper = shallow(<App isLoggedIn={true} />);
+		expect(wrapper.contains(<Login />)).toBe(false);
+	})
+
+	it('Tests that CourseList component is rendered', () => {
+		const wrapper = shallow(<App isLoggedIn />);
+		expect(wrapper.find('CourseList').length).toBe(1);
+	})
+
+	// next 2 tests are under review, spyOn() is not working
+	// and I keep getting this error:
+	// TypeError: wrapper.instance(...).keyDownHandler is not a function
+
+	it(`Verifies that alert is called when ctrl-h is pressed`, () => {
+		jest.spyOn(window, 'alert');
+		const wrapper = shallow(<App isLoggedIn />);
+		const e = {
+			ctrlKey: true,
+			key: 'h'
+		}
+		wrapper.update();
+		wrapper.instance().keyDownHandler(e);
+		expect(window.alert).toHaveBeenCalled();
+	})
+
+	it(`Verifies that logOut function is called when ctrl-h is pressed`, () => {
+		const logOut = jest.fn();
+		const wrapper = shallow(<App isLoggedIn logOut={logOut} />);
+		const e = {
+			ctrlKey: true,
+			key: 'h'
+		}
+		wrapper.instance().keyDownHandler(e);
+		expect(logOut).toHaveBeenCalled();
+	})
+})
