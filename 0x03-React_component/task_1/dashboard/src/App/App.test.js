29ce9f7632c452168b25d9b4dc81f7315e3f9671
Adam Taylor <tayloradam1999@gmail.com>
2022-03-20 15:10:52 -0500

    Repush tests. no checks on checker for what reason?

--- 0x03-React_component/task_1/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.test.js
@@ -49,26 +49,26 @@ describe('<App />', () => {
 	// and I keep getting this error:
 	// TypeError: wrapper.instance(...).keyDownHandler is not a function
 
-	// it(`Verifies that alert is called when ctrl-h is pressed`, () => {
-	// 	jest.spyOn(window, 'alert');
-	// 	const wrapper = shallow(<App isLoggedIn />);
-	// 	const e = {
-	// 		ctrlKey: true,
-	// 		key: 'h'
-	// 	}
-	// 	wrapper.update();
-	// 	wrapper.instance().keyDownHandler(e);
-	// 	expect(window.alert).toHaveBeenCalled();
-	// })
+	it(`Verifies that alert is called when ctrl-h is pressed`, () => {
+		jest.spyOn(window, 'alert');
+		const wrapper = shallow(<App isLoggedIn />);
+		const e = {
+			ctrlKey: true,
+			key: 'h'
+		}
+		wrapper.update();
+		wrapper.instance().keyDownHandler(e);
+		expect(window.alert).toHaveBeenCalled();
+	})
 
-	// it(`Verifies that logOut function is called when ctrl-h is pressed`, () => {
-	// 	const logOut = jest.fn();
-	// 	const wrapper = shallow(<App isLoggedIn logOut={logOut} />);
-	// 	const e = {
-	// 		ctrlKey: true,
-	// 		key: 'h'
-	// 	}
-	// 	wrapper.instance().keyDownHandler(e);
-	// 	expect(logOut).toHaveBeenCalled();
-	// })
+	it(`Verifies that logOut function is called when ctrl-h is pressed`, () => {
+		const logOut = jest.fn();
+		const wrapper = shallow(<App isLoggedIn logOut={logOut} />);
+		const e = {
+			ctrlKey: true,
+			key: 'h'
+		}
+		wrapper.instance().keyDownHandler(e);
+		expect(logOut).toHaveBeenCalled();
+	})
 })
