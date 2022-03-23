911e801297270029cd425c09434bdf6c4641d1ff
Adam Taylor <tayloradam1999@gmail.com>
2022-03-23 17:11:36 -0500

    ESLint

--- 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
@@ -12,25 +12,25 @@ const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
 	const header_row_background_color = { backgroundColor: '#deb5b545' };
 	let node;
 	let style;
-	
+
 	if (isHeader) {
 		style = header_row_background_color;
 		if (!textSecondCell) {
 			node = <th colSpan="2" className={css(rowStyles.th)}>{textFirstCell}</th>;
 		} else {
-			node = 
-		  <React.Fragment>
-			  <th className={css(rowStyles.NOTth)}>{textFirstCell}</th>
-			  <th className={css(rowStyles.NOTth)}>{textSecondCell}</th>
-		  </React.Fragment>;
+			node =
+				<React.Fragment>
+					<th className={css(rowStyles.NOTth)}>{textFirstCell}</th>
+					<th className={css(rowStyles.NOTth)}>{textSecondCell}</th>
+				</React.Fragment>;
 		}
 	} else {
 		style = row_background_color;
-		node = 
-		  <React.Fragment>
-			  <td>{textFirstCell}</td>
-			  <td>{textSecondCell}</td>
-		  </React.Fragment>;
+		node =
+			<React.Fragment>
+				<td>{textFirstCell}</td>
+				<td>{textSecondCell}</td>
+			</React.Fragment>;
 	}
 	return (
 		<tr style={style}>
