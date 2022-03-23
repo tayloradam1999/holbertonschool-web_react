cfe3f1694ff2e7867ce4bf3c91376968294f27a7
Adam Taylor <tayloradam1999@gmail.com>
2022-03-23 11:34:02 -0500

    Remove redunant if statements, refactor PureComponent

--- 0x04-React_inline_styling/task_0/dashboard/src/Notifications/NotificationItem.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/Notifications/NotificationItem.js
@@ -8,10 +8,17 @@ class NotificationItem extends PureComponent {
 		// - type: string, required, default: 'default'
 		// - value: string
 		// - html: object with key '__html' and value: string
-		if ((this.props.type && this.props.value) && (typeof this.props.type === 'string' && typeof this.props.value === 'string') && (!this.props.html)) return(<li data-notification-type={this.props.type} onClick={this.props.markAsRead}>{this.props.value}</li>)
-		if ((!this.props.type) && (this.props.html) && (this.props.html.__html)) return(<li data-notification-type="default" dangerouslySetInnerHTML={this.props.html} onClick={this.props.markAsRead}></li>)
-		if ((this.props.type) && (this.props.html) && (this.props.html.__html)) return(<li data-notification-type={this.props.type} dangerouslySetInnerHTML={this.props.html} onClick={this.props.markAsRead}></li>)
-		return(<li data-notification-type="default" onClick={this.props.markAsRead}>NotificationItem: invalid props</li>)
+		// - markAsRead: function
+		// - id: number
+		const { type, value, html, markAsRead, id } = this.props
+		return (
+			<li onCLick={() => { markAsRead(id) }} 
+			data-notification-type={type}
+			dangerouslySetInnerHTML={html}
+			>
+			{value}
+			</li>
+		)
 	}
 }
 
