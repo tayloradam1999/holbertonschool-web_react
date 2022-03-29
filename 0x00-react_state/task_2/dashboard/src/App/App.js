246e4080af1814d809fd5ea172b7d38ad38daa9d
Adam Taylor <tayloradam1999@gmail.com>
2022-03-29 17:10:38 -0500

    Completed task 2

---
+++ 0x00-react_state/task_2/dashboard/src/App/App.js
@@ -0,0 +1,150 @@
+import React, { Component } from 'react'
+import { StyleSheet, css } from 'aphrodite'
+import { AppProvider } from './AppContext'
+import Notifications from '../Notifications/Notifications'
+import { getLatestNotification } from '../utils/utils'
+import Login from '../Login/Login'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+import CourseList from '../CourseList/CourseList'
+import BodySection from '../BodySection/BodySection'
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom'
+
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			displayDrawer: false,
+			user: {
+				email: '',
+				password: '',
+				isLoggedIn: false
+			},
+			logOut: this.logOut
+		}
+	};
+
+
+	componentDidMount() {
+		window.addEventListener('keydown', this.keyDownHandler);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('keydown', this.keyDownHandler);
+	}
+
+	keyDownHandler = (e) => {
+		if (e.keyCode === 72 && e.ctrlKey) {
+			alert('Logging you out');
+			this.state.logOut();
+		}
+	}
+
+	handleDisplayDrawer = () => {
+		this.setState({ displayDrawer: true })
+	}
+
+	handleHideDrawer = () => {
+		this.setState({ displayDrawer: false })
+	}
+
+	logIn = (email, password) => {
+		this.setState({
+			user: {
+				email,
+				password,
+				isLoggedIn: true
+			}
+		})
+	}
+
+	logOut = () => {
+		this.setState({
+			user: {
+				email: '',
+				password: '',
+				isLoggedIn: false
+			}
+		})
+	}
+
+	render() {
+		const { displayDrawer } = this.state;
+
+		const listCourses = [
+			{ id: 1, name: 'ES6', credit: '60' },
+			{ id: 2, name: 'Webpack', credit: '20' },
+			{ id: 3, name: 'React', credit: '40' }
+		]
+
+		const listNotifications = [
+			{ id: 1, type: "default", value: "New course available" },
+			{ id: 2, type: "urgent", value: "New resume available" },
+			{ id: 3, html: { __html: getLatestNotification() }, type: "urgent" }
+		]
+
+		return (
+			<AppProvider value={{
+				displayDrawer: displayDrawer,
+				handleDisplayDrawer: this.handleDisplayDrawer,
+				handleHideDrawer: this.handleHideDrawer,
+				user: this.state.user,
+				logIn: this.logIn,
+				logOut: this.logOut
+			}}>
+				<div className={css(bodyStyles.App)}>
+					<Notifications
+						listNotifications={listNotifications}
+						displayDrawer={displayDrawer}
+						handleDisplayDrawer={this.handleDisplayDrawer}
+						handleHideDrawer={this.handleHideDrawer}
+					/>
+					<Header />
+					<div className="App-body">
+						{this.state.user.isLoggedIn
+							?
+							<BodySectionWithMarginBottom title="Course list">
+								<CourseList listCourses={listCourses} />
+							</BodySectionWithMarginBottom>
+							:
+							<BodySectionWithMarginBottom title="Login in to continue">
+								<Login logIn={this.logIn} />
+							</BodySectionWithMarginBottom>
+						}
+						<BodySection title="News from the School"><p>Boring random text</p></BodySection>
+					</div>
+					<div className={css(footerStyles.Footer)}>
+						<Footer />
+					</div>
+				</div>
+			</AppProvider>
+		)
+	}
+}
+
+
+const primaryColor = '#E11D3F';
+
+const bodyStyles = StyleSheet.create({
+	App: {
+		backgroundColor: '#ffffff',
+		display: 'flex',
+		flexDirection: 'column',
+	}
+});
+
+const footerStyles = StyleSheet.create({
+	Footer: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderTop: `3px solid ${primaryColor}`,
+		padding: '1rem',
+		fontStyle: 'italic',
+	}
+});
+
+
+export default App
---
+++ 0x00-react_state/task_2/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,152 @@
+import React, { Component } from 'react'
+import { StyleSheet, css } from 'aphrodite'
+import close_icon from '../assets/close-icon.png'
+import NotificationItem from './NotificationItem'
+import NotificationItemShape from './NotificationItemShape'
+import propTypes from 'prop-types'
+
+
+class Notification extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	markAsRead(id) {
+		console.log(`Notification ${id} has been read`);
+	}
+
+	// function that makes the file only update when next listNotifications is longer than current
+	shouldComponentUpdate(nextProps) {
+		// take into account that the component needs to be able
+		// to rerender when the prop <displayDrawer> changes
+		if (nextProps.displayDrawer !== this.props.displayDrawer) return true;
+		// if the listNotifications is not empty
+		if (nextProps.listNotifications.length > 0) {
+			// if the listNotifications is not the same as the current listNotifications
+			if (nextProps.listNotifications !== this.props.listNotifications) return true;
+		}
+		return false;
+	}
+
+	render() {
+		// assign props to local variables
+		const { 
+			listNotifications,
+			displayDrawer,
+			handleDisplayDrawer,
+			handleHideDrawer
+		} = this.props;
+
+		return (
+			<React.Fragment>
+				<div className={css(notificationStyles.pDiv)}>
+					<p 
+						className={css(animationStyle.animation)}
+						id="notiP"
+						onClick={handleDisplayDrawer}
+					>
+					Your notifications</p>
+				</div>
+				{displayDrawer && (
+					<div className={css(notificationStyles.notifications)}>
+						<button style={{
+							position: 'absolute',
+							background: 'transparent',
+							border: 'none',
+							right: '20px',
+						}}
+							aria-label='close'
+							onClick={() => {
+								console.log('Close button has been clicked');
+							}}>
+							<img
+								src={close_icon}
+								className={css(notificationStyles.x_button)}
+								alt="close" height="15px" width="15px"
+								onClick={handleHideDrawer}
+								id="x_button">
+							</img>
+						</button>
+						<p>Here is the list of notifications</p>
+						<ul>
+							{/* listNotifications is empty condition */}
+							{listNotifications.length === 0 && (
+								<li>
+									<p>No notification available yet</p>
+								</li>
+							)}
+							{/* render listNotifications */}
+							{listNotifications.map(notification => (
+								<NotificationItem key={notification.id} type={notification.type} value={notification.value} html={notification.html} markAsRead={this.markAsRead} id={notification.id} />
+							))}
+						</ul>
+					</div>
+				)}
+			</React.Fragment>
+		)
+	}
+}
+
+const notificationStyles = StyleSheet.create({
+	notifications: {
+		'@media (min-width: 350px)': {
+			position: 'absolute',
+			top: '0px',
+			left: '0px',
+			width: '100%',
+			height: '100%',
+			background: 'white',
+			fontSize: '20px',
+		}
+	},
+
+	x_button: {
+		'@media (min-width: 350px)': {
+			position: 'absolute',
+			top: '15px',
+			right: '10px',
+		}
+	},
+
+	pDiv: {
+		position: 'absolute',
+		top: `0px`,
+		right: `15px`,
+		backgroundColor: '#fff8f8',
+	},
+})
+
+const opacityAnimation = {
+	'0%': { opacity: 0 },
+	'100%': { opacity: 1 },
+}
+
+const translateYAnimation = {
+	'0%': { transform: 'translateY(0px)' },
+	'50%': { transform: 'translateY(-10px)' },
+}
+
+const animationStyle = StyleSheet.create({
+	animation: {
+		animationName: [opacityAnimation, translateYAnimation],
+		animationDuration: '3s, 1200ms',
+		animationIterationCount: '1, 3'
+	}
+})
+
+
+Notification.defaultProps = {
+	displayDrawer: false,
+	listNotifications: [],
+	handleDisplayDrawer: () => { },
+	handleHideDrawer: () => { },
+}
+
+Notification.propTypes = {
+	displayDrawer: propTypes.bool,
+	listNotifications: propTypes.arrayOf(NotificationItemShape),
+	handleDisplayDrawer: propTypes.func,
+	handleHideDrawer: propTypes.func,
+}
+
+export default Notification
