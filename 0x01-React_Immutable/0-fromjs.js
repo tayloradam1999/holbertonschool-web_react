947d5a79167a4ec94a217cc6dc15ff4288a1028d
Adam Taylor <tayloradam1999@gmail.com>
2022-04-03 11:55:35 -0500

    Edits for ESLINT and fromJS

--- 0x01-React_Immutable/0-fromjs.js
+++ 0x01-React_Immutable/0-fromjs.js
@@ -1,7 +1,11 @@
 import Immutable from 'immutable';
 
-
 // takes object and returns it as an immutable Map object
 export default function getImmutableObject(object) {
-	return Immutable.Map(object);
+  return Immutable.fromJS(object, function (key, value) {
+    if (Immutable.Iterable.isIterable(value)) {
+      return value.toMap();
+    }
+    return value;
+  })
 }
