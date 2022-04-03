c2c65e7a7bbd6b7621388006a1105f23aa4e15ed
Adam Taylor <tayloradam1999@gmail.com>
2022-04-03 13:31:39 -0500

    Task 7

---
+++ 0x01-React_Immutable/7-equality.js
@@ -0,0 +1,11 @@
+import Immutable from 'immutable';
+
+function areMapsEqual(map1, map2) {
+  // Compares two immutable maps and returns true if they are equal
+  // map1 - immutable map
+  // map2 - immutable map
+  // returns true if maps are equal, false otherwise
+  return map1.is(map2);
+}
+
+export { areMapsEqual };
