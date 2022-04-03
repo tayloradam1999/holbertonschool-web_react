782a9b99d86d2d817d5849654e3563a7eeddb240
Adam Taylor <tayloradam1999@gmail.com>
2022-04-03 13:29:06 -0500

    Task 6

---
+++ 0x01-React_Immutable/6-deeply.js
@@ -0,0 +1,15 @@
+import Immutable from 'immutable';
+
+function mergeDeeplyElements(page1, page2) {
+  // Deeply merges page2 to page1
+  // page1 - object
+  // page2 - object
+  // Returns a list containing the values of page1 and page2
+  // if two values are the same, they should combine.
+  const myMap = Immutable.Map(page1);
+  const myMap2 = Immutable.Map(page2);
+  return myMap.mergeDeep(myMap2);
+}
+
+
+export { mergeDeeplyElements };
