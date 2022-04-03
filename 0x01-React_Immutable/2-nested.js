60792400176b4eca0bffdbe3ea1996237ae588eb
Adam Taylor <tayloradam1999@gmail.com>
2022-04-03 14:11:51 -0500

    Refactor and add error clause to getIn

--- 0x01-React_Immutable/2-nested.js
+++ 0x01-React_Immutable/2-nested.js
@@ -1,23 +1,9 @@
-import { getIn, Map } from 'immutable';
+import { Map } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
   // object - plain object
   // array - array containing list of a path to some key in object
-  //
   // Returns value of object at defined path
-  // Return types:
-  // - Immutable.Map
-  // - String
-  // - Undefined
-  if (!object || !array || !typeof object === 'object' || !Array.isArray(array)) {
-    return undefined;
-  }
-  let myMap = Map(object);
-  for (let i = 0; i < array.length; i++) {
-    myMap = getIn(myMap, array[i]);
-    if (myMap === undefined) {
-      return undefined;
-    }
-  }
-  return myMap;
+  const myMap = Map(object);
+  return myMap.getIn(array, undefined);
 }
