af0aff20345ed114baac5734c26d093cc76d3781
Adam Taylor <tayloradam1999@gmail.com>
2022-04-03 14:00:14 -0500

    Fix seq typo to Seq

--- 0x01-React_Immutable/8-seq.js
+++ 0x01-React_Immutable/8-seq.js
@@ -1,11 +1,11 @@
-import { seq } from 'immutable';
+import { Seq } from 'immutable';
 
 function printBestStudents(students) {
   // Filters students by score < 70 and console.logs their sequence
   // with the first letter of their 'firstName' and 'lastName' capitalized
   // students - Object
   // returns nothing
-  const myObj = seq(students);
+  const myObj = Seq(students);
   const myList = myObj.filter(student => student.get('score') < 70);
   myList.forEach(student => {
     student.firstName = student.charAt(0).toUpperCase() + student.slice(1);
