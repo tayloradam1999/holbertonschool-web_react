fcd43df8894bff242d50ba8d43bf1000b476ee9c
Adam Taylor <tayloradam1999@gmail.com>
2022-04-03 12:47:23 -0500

    Task 3

---
+++ 0x01-React_Immutable/3-list.js
@@ -0,0 +1,11 @@
+import Immutable from 'immutable';
+
+export function getListObject(array) {
+  // returns array as a immutable List object
+  return Immutable.List(array);
+}
+
+export function addElementToList(list, element) {
+  // appends list to string and returns list
+  return list.push(element);
+}
