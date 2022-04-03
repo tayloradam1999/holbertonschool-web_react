e07cea3ea448ee6cd140e95633e68cadd212438e
Adam Taylor <tayloradam1999@gmail.com>
2022-04-03 13:50:23 -0500

    Refactor is method call

--- 0x01-React_Immutable/7-equality.js
+++ 0x01-React_Immutable/7-equality.js
@@ -1,15 +1,11 @@
-import Immutable from 'immutable';
+import { is } from 'immutable';
 
 function areMapsEqual(map1, map2) {
   // Compares two immutable maps and returns true if they are equal
   // map1 - immutable map
   // map2 - immutable map
   // returns true if maps are equal, false otherwise
-  if (map1.Immutable.is(map2)) {
-    return true;
-  } else {
-    return false;
-  }
+  return is(map1, map2);
 }
 
 export default areMapsEqual;
