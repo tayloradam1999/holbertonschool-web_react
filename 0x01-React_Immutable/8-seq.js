f041eccb8a1bfd1a73408a8e2f3b43ef92158fad
Adam Taylor <tayloradam1999@gmail.com>
2022-04-03 13:53:15 -0500

    Refactor and implement seq

--- 0x01-React_Immutable/8-seq.js
+++ 0x01-React_Immutable/8-seq.js
@@ -1,14 +1,15 @@
-import Immutable from 'immutable';
+import { seq } from 'Immutable';
 
 function printBestStudents(students) {
   // Filters students by score < 70 and console.logs their sequence
   // with the first letter of their 'firstName' and 'lastName' capitalized
   // students - Object
   // returns nothing
-  const mySeq = students.filter(student => student.score < 70);
-  mySeq.forEach(student => {
-    student.firstName = student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1);
-    student.lastName = student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1);
+  const myObj = seq(students);
+  const myList = myObj.filter(student => student.get('score') < 70);
+  myList.forEach(student => {
+    student.firstName = student.charAt(0).toUpperCase() + student.slice(1);
+    student.lastName = student.charAt(0).toUpperCase() + student.slice(1);
     console.log(student);
   });
 }
