05f955c2e4d689665541a0c01065517c92f1d9e9
Adam Taylor <tayloradam1999@gmail.com>
2022-04-03 13:25:36 -0500

    Change spacing to 2 spaces instead of 4

--- 0x01-React_Immutable/5-merge.js
+++ 0x01-React_Immutable/5-merge.js
@@ -1,24 +1,24 @@
 import Immutable from 'immutable';
 
 function concatElements(page1, page2) {
-	// concatenates page2 to page1
-	// page1 - array
-	// page2 - array
-	// Returns a list containing the values of page1 and page2
-	const myList = Immutable.List(page1);
-	const myList2 = Immutable.List(page2);
-	return myList.concat(myList2);
+  // concatenates page2 to page1
+  // page1 - array
+  // page2 - array
+  // Returns a list containing the values of page1 and page2
+  const myList = Immutable.List(page1);
+  const myList2 = Immutable.List(page2);
+  return myList.concat(myList2);
 }
 
 function mergeElements(page1, page2) {
-	// merges page2 to page1
-	// page1 - object
-	// page2 - object
-	// Returns a list containing the values of page1 and page2
-	// if two values are the same, page2 value will be used.
-	const myMap = Immutable.Map(page1);
-	const myMap2 = Immutable.Map(page2);
-	return myMap.merge(myMap2);
+  // merges page2 to page1
+  // page1 - object
+  // page2 - object
+  // Returns a list containing the values of page1 and page2
+  // if two values are the same, page2 value will be used.
+  const myMap = Immutable.Map(page1);
+  const myMap2 = Immutable.Map(page2);
+  return myMap.merge(myMap2);
 }
 
 
