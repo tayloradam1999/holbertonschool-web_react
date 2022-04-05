136b8e430fb1ced9f8d2ae3248317b3715280774
Adam Taylor <tayloradam1999@gmail.com>
2022-04-05 16:52:17 -0500

    Split tests into 2 describe cases

--- 0x02-react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.test.js
+++ 0x02-react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.test.js
@@ -8,7 +8,9 @@ describe("selectCourse", () => {
       index: 1
     });
   });
+})
 
+describe("unselectCourse", () => {
   it(`Tests calling unselectCourse with 1 as argument`, () => {
     expect(unselectCourse(1)).toEqual({
       type: 'UNSELECT_COURSE',
